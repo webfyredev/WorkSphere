a1f67fb43d69f305a33516e3fb64c5ce11577908
impphilwebs <imphilwebs@gmail.com>
2025-10-04 09:49:23 +0100

    Progress on testimonial sections

--- src/components/testimonial.jsx
+++ src/components/testimonial.jsx
@@ -7,7 +7,8 @@ import 'swiper/css/navigation'
 import { useState, useEffect } from "react";
 import { Star } from "lucide-react";
 import { motion } from "framer-motion";
-import { ScrollUp } from "../animations/motion";
+import { ScrollUp, CardHover} from "../animations/motion";
+import '../index.css'
 export default function Testimonial(){
     
    return(
@@ -15,13 +16,13 @@ export default function Testimonial(){
         <>
         <motion.h2 {...ScrollUp} className="text-2xl font-semibold mt-20 w-full h-10 flex justify-center items-center text-blue-500">Our Clients Reports</motion.h2>
         <p className="w-full h-10 flex items-center justify-center text-gray-400 text-sm">Lorem ipsum dolor sit amet consectetur adipisicing elit. Quos, fugiat!</p>
-        <div className="w-full max-w-6xl mx-auto px-4 py-20 relative  mb-5">
+        <div className="w-full mx-auto px-5 py-10 relative mb-5">
         <Swiper 
             modules={[Autoplay, Pagination, Navigation]}
-            spaceBetween={20}
+            spaceBetween={10}
             slidesPerView={3}
             loop = {true}
-            autoplay = {{delay : 3000, disableOnInteraction : false}}
+            autoplay = {{delay : 5000, disableOnInteraction : false}}
             pagination = {{clickable : true}}
             navigation = {true}
             breakpoints={{
@@ -30,12 +31,11 @@ export default function Testimonial(){
                 1024 : {slidesPerView : 3},
                 
             }}
-            className="pb-12"
         >
             {testimonials.map((data, index) =>(
                 <SwiperSlide key={index}>
-                    <div className="bg-white w-[100%] shadow- shadow-md rounded-2xl p-6 flex flex-col items-center justify-center text-center h-80 cursor-pointer">
-                        <img src={data.img} className="w-16 h-16 rounded-full mb-4" />
+                    <motion.div {...CardHover} className="bg-white w-[90%] shadow- shadow-[0_0_10px_rgba(0,0,0,0.1)] rounded-2xl p-6 flex flex-col items-center justify-center text-center h-75 mb-15 cursor-pointer">
+                        <img src={data.img} className="w-16 h-16 rounded-full mb-4 object-cover" />
                         <p className="text-gray-600 mb-4 text-sm">{data.text}</p>
                         <h3 className="font-semibold text-sm text-blue-600">{data.name}</h3>
                         <div className="flex flex-row justify-center mb-3 mt-2">
@@ -43,14 +43,13 @@ export default function Testimonial(){
                                 <Star key={i} size={15} className={i < data.rating ? "text-blue-500 fill-blue-500" : "text-blue-500"}/>
                             ))}
                         </div>
-                    </div>
+                    </motion.div>
                 </SwiperSlide>
             ))}
 
         </Swiper>
         {/* Custom Styles for Swiper */}
       <style jsx>{`
-        /* Pagination dots */
         .swiper-pagination-bullet {
           @apply bg-gray-300 w-3 h-3 rounded-full opacity-100;
         }
@@ -61,31 +60,14 @@ export default function Testimonial(){
         /* Navigation arrows */
         .swiper-button-next,
         .swiper-button-prev {
-          @apply bg-white shadow-md rounded-full w-10 h-10 flex items-center justify-center text-green-600;
+          @apply bg-white shadow-md rounded-full w-5 h-5 flex items-center justify-center text-green-600;
         }
         .swiper-button-next::after,
         .swiper-button-prev::after {
           @apply text-lg font-bold;
         }
-      `}</style>
-        {/* <style jsx>
-            {`
-                .swiper-pagination-bullet {
-                    @apply bg-gray-700 w-3 h-3 rounded-full opacity-100;
-                    }
-                .swiper-pagination-bullet-active {
-                    @apply bg-green-600 w-4 h-4;
-                    }
-                .swiper-button-next, 
-                .swiper-button-prev{
-                @apply bg-white shadow-md rounded-full w-2 h-2 flex items-center justify-center
-                }
-                .swiper-button-next::after,
-                .swiper-button-prev::after {
-                @apply text-lg font-bold;
-                }
-            `}
-        </style> */}
+      `}
+      </style>
         </div>
             
     </>
--- src/data/testimonial.js
+++ src/data/testimonial.js
@@ -1,4 +1,9 @@
-import testImg from '../images/banner-2.jpg'
+import testImg from '../images/testimonial/test (1).jpg'
+import testImg2 from '../images/testimonial/test (2).jpg'
+import testImg3 from '../images/testimonial/test (3).jpg'
+import testImg4 from '../images/testimonial/test (4).jpg'
+import testImg5 from '../images/testimonial/test (5).jpg'
+
 
 const testimonials = [
     {
@@ -10,25 +15,25 @@ const testimonials = [
     {
         name : 'PAUL PINCHAI',
         text : 'Lorem, ipsum dolor sit amet consectetur adipisicing elit. Deleniti blanditiis saepe mollitia.',
-        img : testImg,
+        img : testImg2,
         rating : 5
     },
     {
         name : 'ADAM JINNA',
         text : 'Lorem, ipsum dolor sit amet consectetur adipisicing elit. Deleniti blanditiis saepe mollitia.',
-        img : testImg,
+        img : testImg3,
         rating: 4
     },
     {
         name : 'LISA GREEN',
         text : 'Lorem, ipsum dolor sit amet consectetur adipisicing elit. Deleniti blanditiis saepe mollitia.',
-        img : testImg,
+        img : testImg4,
         rating: 5
     },
     {
         name : 'MARK JOHN',
         text : 'Lorem, ipsum dolor sit amet consectetur adipisicing elit. Deleniti blanditiis saepe mollitia.',
-        img : testImg,
+        img : testImg5,
         rating : 4
     }
 ]
